3af8d9d9f51d2eee5c9b3a68a931bd5e767f1024
Reuben <gathiireuben@gmail.com>
2023-08-24 06:35:17 +0300

    Added new font in typography

--- app/src/main/java/com/example/woofapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/woofapp/ui/theme/Type.kt
@@ -2,16 +2,42 @@ package com.example.woofapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.woofapp.R
 
 // Set of Material typography styles to start with
+val AbrilFatface = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    body1 = TextStyle( // displayLarge
+        fontFamily = AbrilFatface,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 36.sp
+    ),
+    body2 = TextStyle( //displayMedium
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    subtitle1 = TextStyle( //labelSmall
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    subtitle2 = TextStyle( //bodyLarge
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
     /* Other default text styles to override
     button = TextStyle(
