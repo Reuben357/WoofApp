52e75d78b3f4d5a23196c1e44b87111e299f107d
Reuben <gathiireuben@gmail.com>
2023-08-24 06:37:46 +0300

    Changed colors in dark and light palettes

--- app/src/main/java/com/example/woofapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/woofapp/ui/theme/Theme.kt
@@ -1,21 +1,29 @@
 package com.example.woofapp.ui.theme
 
+import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = md_theme_dark_primary,
+    primaryVariant = md_theme_dark_onPrimary,
+    secondary = md_theme_dark_onPrimary
 )
 
+
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = md_theme_light_primary,
+    primaryVariant = md_theme_light_primaryContainer,
+    secondary = md_theme_light_secondary
 
     /* Other default colors to override
     background = Color.White,
@@ -28,7 +36,10 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun WoofAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun WoofAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
