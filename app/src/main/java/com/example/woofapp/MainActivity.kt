46aec51f66f9ee4ba1dd7326600b242a653e0720
Reuben <gathiireuben@gmail.com>
2023-08-24 06:23:35 +0300

    Added a scaffold layout to hold the topBar, created a topBar composable with Image and Text, Added an expandable Icon and animation, Added expanded information on dog hobbies and a DogItemButton composable

--- app/src/main/java/com/example/woofapp/MainActivity.kt
+++ app/src/main/java/com/example/woofapp/MainActivity.kt
@@ -5,15 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -30,7 +37,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
                 ) {
                     WoofApp()
                 }
@@ -41,19 +47,65 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WoofApp(){
-    LazyColumn{
-        items(dogs){
-            DogItem(dog = it)
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ) {
+        it ->
+        LazyColumn(contentPadding = it){
+            items(dogs){
+                DogItem(
+                    dog = it,
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))
+                )
+            }
         }
     }
 
+
 }
+
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier
+                ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_woof_logo),
+
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
+
+
+
 
 @Composable
 fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessMedium
+                )
+            )
+    ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
@@ -61,6 +113,64 @@ fun DogItem(
     ) {
         DogIcon(dog.imageResourceId)
         DogInformation(dog.name, dog.age)
+        Spacer(modifier = Modifier.weight(1f))
+        DogItemButton(
+            expanded = expanded,
+            onClick = { expanded = !expanded }
+        )
+    }
+        if (expanded){
+        DogHobby(
+            dog.hobbies,
+            modifier = Modifier.padding(
+                start = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_small),
+                end = dimensionResource(R.dimen.padding_medium),
+                bottom = dimensionResource(R.dimen.padding_medium)
+            )
+        )
+        }
+}
+}
+
+@Composable
+fun DogHobby(
+    @StringRes dogHobby: Int,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.subtitle1
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if (expanded){
+                Icons.Filled.KeyboardArrowUp
+            } else {
+                Icons.Filled.KeyboardArrowDown
+            },
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colors.secondary
+        )
     }
 }
 
@@ -72,7 +182,11 @@ fun DogIcon(
     Image(
         modifier = modifier
             .size(dimensionResource(R.dimen.image_size))
-            .padding(dimensionResource(R.dimen.padding_small)),
+            .padding(dimensionResource(R.dimen.padding_small))
+            .clip(MaterialTheme.shapes.small),
+
+        //        To make all the photos circular, add in a ContentScale and a Crop attribute; this crops the image to fit.
+        contentScale = ContentScale.Crop,
         painter = painterResource(dogIcon),
         contentDescription = null
     )
@@ -86,18 +200,21 @@ fun DogInformation(
     Column(modifier = modifier) {
         Text(
             text = stringResource(dogName),
-            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small))
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_small)),
+            style = MaterialTheme.typography.body2
         )
         Text(
             text = stringResource(R.string.years_old, dogAge),
+            style = MaterialTheme.typography.subtitle2
+
         )
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun WoofPreview() {
-    WoofAppTheme(darkTheme = false) {
+    WoofAppTheme {
         WoofApp()
     }
 }
